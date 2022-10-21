2d68c5d252b7ef8353ea205e9b52f3106eaa8e21
Oliver Little <oliver.little12@gmail.com>
2022-10-21 13:00:17 +0100

    Add Python RPC API

---
+++ client/rpc/client_service.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import grpc
+import protobuf.table_model_pb2 as protobuf_model
+import protobuf.client_query_pb2_grpc as client_query
+
+class ClientQueryManager():
+    def __init__(self):
+        self.grpc_channel = None
+        self.stub = None
+
+    def open():
+        raise NotImplementedError("ClientQueryManager is a base class, so this is not implemented.")
+
+    def close():
+        raise NotImplementedError("ClientQueryManager is a base class, so this is not implemented.")
+
+    def send_table(self, table : protobuf_model.Table) -> protobuf_model.TableComputeResult:
+        response = self.stub.SendTable(table)
+        return response
+
+class InsecureClientQueryManager(ClientQueryManager):
+    def __init__(self, address : str):
+        super().__init__
+        self.address = address
+
+    def open(self) -> InsecureClientQueryManager:
+        if self.grpc_channel is None:
+            self.grpc_channel = grpc.insecure_channel(self.address)
+            self.stub = client_query.TableClientServiceStub9
+        return self
+        
+
+    def close(self) -> bool:
+        try:
+            self.grpc_channel.close()
+            return True
+        except AttributeError:
+            print("Channel already closed.")
+            return True
+
+    def __enter__(self):
+        self.open()
+    
+    def __exit__(self, exception_type, exception_val, trace):
+        self.close()
