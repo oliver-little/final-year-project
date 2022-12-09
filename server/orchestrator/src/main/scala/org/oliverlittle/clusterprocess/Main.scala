18c2c966c0f68742a85471164c75cb5fa87fd021
Oliver Little <oliver.little12@gmail.com>
2022-12-09 16:13:00 +0000

    Fix syntax error

--- server/orchestrator/src/main/scala/org/oliverlittle/clusterprocess/Main.scala
+++ server/orchestrator/src/main/scala/org/oliverlittle/clusterprocess/Main.scala
@@ -11,7 +11,7 @@ import org.oliverlittle.clusterprocess.table_model._
 @main def main: Unit = {
     val source = CassandraDataSource("localhost", "test", "test_table")
     val tokenRange = CassandraTokenRange(1, 1000)
-    val table = Table("data_source", Seq(SelectTransformation(FieldOperations.AddInt(V(1), V(2)).as("Test_Field")).protobuf))
+    val table = Table(Seq(SelectTransformation(FieldOperations.AddInt(V(1), V(2)).as("Test_Field")).protobuf))
     val computePartialResultCassandraRequest = ComputePartialResultCassandraRequest(Some(table), Some(source), Some(tokenRange))
 
     val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext.build;
