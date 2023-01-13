35ac008d749cd279a0abfdfe823cca8424c9ed95
oliver-little <oliver.little12@gmail.com>
2023-01-13 11:32:03 +0000

    Add Cassandra connection step to startup

--- server/orchestrator/src/main/scala/org/oliverlittle/clusterprocess/Main.scala
+++ server/orchestrator/src/main/scala/org/oliverlittle/clusterprocess/Main.scala
@@ -1,24 +1,10 @@
 package org.oliverlittle.clusterprocess
 
-import io.grpc.ManagedChannelBuilder
-
-import org.oliverlittle.clusterprocess.model.table.SelectTransformation
-import org.oliverlittle.clusterprocess.model.field.expressions._
-import org.oliverlittle.clusterprocess.data_source._
-import org.oliverlittle.clusterprocess.worker_query._
-import org.oliverlittle.clusterprocess.table_model._
 import org.oliverlittle.clusterprocess.server.ClientQueryServer
+import org.oliverlittle.clusterprocess.cassandra.CassandraConnector
 
 @main def main: Unit = {
-    /*
-    val source = CassandraDataSource("localhost", "test", "test_table")
-    val tokenRange = CassandraTokenRange(1, 1000)
-    val table = Table(Seq(SelectTransformation(FieldOperations.AddInt(V(1), V(2)).as("Test_Field")).protobuf))
-    val computePartialResultCassandraRequest = ComputePartialResultCassandraRequest(Some(table), Some(source), Some(tokenRange))
-
-    val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext.build;
-    val blockingStub = WorkerComputeServiceGrpc.blockingStub(channel);
-    val res = blockingStub.computePartialResultCassandra(computePartialResultCassandraRequest);
-    */
+    // Verify connection to force a failure at startup if Cassandra is unavailable
+    CassandraConnector.verifyConnection
     ClientQueryServer.main()
 }
