aba612327c68412b32833d433e5f453de5c3e81b
oliver-little <oliver.little12@gmail.com>
2023-01-13 11:33:48 +0000

    Further cleanup kubernetes files

--- kubernetes/push_to_cluster.ps1
+++
@@ -1,4 +0,0 @@
-docker tag worker localhost:5000/worker
-docker push localhost:5000/worker
-docker tag orchestrator localhost:5000/orchestrator
-docker push localhost:5000/orchestrator
--- kubernetes/start.ps1
+++
@@ -1,2 +0,0 @@
-kubectl apply -f .\configs\
-helm install cassandra-release bitnami/cassandra --set service.type=NodePort --set dbUser.forcePassword=false
