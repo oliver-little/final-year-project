1feff8cff3da3e90bfa1c5951d03f9c9fa345e90
oliver-little <oliver.little12@gmail.com>
2023-01-13 10:06:57 +0000

    Add authentication and config through environment variables to CassandraConnector

--- server/core/src/main/scala/org/oliverlittle/clusterprocess/connector/cassandra/CassandraConnector.scala
+++ server/core/src/main/scala/org/oliverlittle/clusterprocess/connector/cassandra/CassandraConnector.scala
@@ -2,12 +2,26 @@ package org.oliverlittle.clusterprocess.cassandra
 
 import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.metadata.schema.TableMetadata
+import com.datastax.oss.driver.api.core.auth.{AuthProvider, ProgrammaticPlainTextAuthProvider}
 import java.net.InetSocketAddress
+import scala.io.Source.fromFile
+import scala.util.Properties.{envOrElse, envOrNone}
 
 object CassandraConnector {
-    var host : String = "localhost"
-    var datacenter : String = "datacenter1"
-    private var session : CqlSession = CqlSession.builder().addContactPoint(new InetSocketAddress(host, 9042)).withLocalDatacenter(datacenter).build()
+    var host : String = envOrElse("CASSANDRA_URL", "localhost")
+    var port : Int = envOrElse("CASSANDRA_PORT", "9042").toInt
+    var datacenter : String = envOrElse("CASSANDRA_DATACENTER", "datacenter1")
+
+    var usernameFile : Option[String] = envOrNone("CASSANDRA_USERNAME_FILE")
+    var passwordFile : Option[String] = envOrNone("CASSANDRA_PASSWORD_FILE")
+    // Get username and password from file if they exist
+    var username : Option[String] = usernameFile.flatMap(f => Some(fromFile(f).getLines.mkString))
+    var password : Option[String] = passwordFile.flatMap(f => Some(fromFile(f).getLines.mkString))
+    var authProvider : Option[AuthProvider] = if username.isDefined && password.isDefined then Some(new ProgrammaticPlainTextAuthProvider(username.get, password.get)) else None
+
+    private var sessionBuilder = CqlSession.builder().addContactPoint(new InetSocketAddress(host, port)).withLocalDatacenter(datacenter)
+    if authProvider.isDefined then sessionBuilder.withAuthProvider(authProvider.get)
+    private var session : CqlSession = sessionBuilder.build()
 
     def getSession : CqlSession = session
 
