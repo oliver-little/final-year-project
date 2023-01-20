04320d95e607e7aa7caaa22c51cbed994c6db6d6
Oliver Little <oliver.little12@gmail.com>
2023-01-20 18:43:33 +0000

    Fix error in TableSizeEstimator

--- server/core/src/main/scala/org/oliverlittle/clusterprocess/connector/cassandra/size_estimation/TableSizeEstimator.scala
+++ server/core/src/main/scala/org/oliverlittle/clusterprocess/connector/cassandra/size_estimation/TableSizeEstimator.scala
@@ -9,7 +9,7 @@ import collection.JavaConverters._
 object TableSizeEstimation {
     def estimateTableSize(session : CqlSession, keyspace : String, table : String) : TableSizeEstimation = {
         val res = session.execute("SELECT keyspace_name, table_name, range_start, range_end, mean_partition_size, partitions_count FROM system.size_estimates WHERE keyspace_name=? AND table_name=?", keyspace, table)
-        res.iterator.asScala.map(row => SizeEstimatesRow(CassandraTokenRange.fromLong(session.getMetadata.getTokenMap.get, row.getLong("range_start"), row.getLong("range_end"), row.getLong("mean_partition_size"), row.getLong("partitions_count"))))
+        return TableSizeEstimation(res.iterator.asScala.map(row => SizeEstimatesRow(CassandraTokenRange.fromLong(session.getMetadata.getTokenMap.get, row.getLong("range_start"), row.getLong("range_end")), row.getLong("mean_partition_size"), row.getLong("partitions_count"))).toSeq)
     }
 }
 
