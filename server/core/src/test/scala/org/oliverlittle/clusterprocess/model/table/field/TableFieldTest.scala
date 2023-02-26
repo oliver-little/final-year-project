8da1c3e0241cefaf03ee4b630af1336d855cbe83
oliver-little <oliver.little12@gmail.com>
2023-02-26 11:53:50 +0000

    Add test stubs for TableField

---
+++ server/core/src/test/scala/org/oliverlittle/clusterprocess/model/table/field/TableFieldTest.scala
@@ -0,0 +1,30 @@
+package org.oliverlittle.clusterprocess.model.table.field
+
+package org.oliverlittle.clusterprocess.model.table.field
+
+import org.oliverlittle.clusterprocess.UnitSpec
+
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito._
+import org.scalatestplus.mockito.MockitoSugar
+
+class TableField extends UnitSpec {
+    "A TableField" should "convert from protobuf to BaseTableField" in {
+        fail()
+    }
+}
+
+class TableValue extends UnitSpec {
+    "A TableValue" should "convert from protobuf" in {
+        fail()
+    }
+
+    it should "convert to protobuf when it has a value" in {
+        fail()
+    }
+
+    it should "convert to a null protobuf value when it is None" in {
+        fail()
+    }
+}
